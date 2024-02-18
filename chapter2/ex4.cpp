d70f8e9370214e6f5e64aac8b4a48ffb32f2c236
radwan <radwan.al.shawesh@gmail.com>
2024-02-18 22:35:13 +0300

    add

--- chapter2/ex4.cpp
+++ chapter2/ex4.cpp
@@ -2,5 +2,21 @@
 
 int main()
 {
-    int c{0}, thisIsAVariable{0}, q76354{0}, number{0};
+    int d{0}, thisIsAVariable{0}, q76354{0}, number{0};
+    int age{};
+    std::cout << "Enter an Integer: ";
+    std::cin >> age;
+    if (age != 7)
+    {
+        std::cout << "The variable number is not equal to 7\n";
+    }
+    std::cout << "This is a C++ program\n";
+    std::cout << "This is a C++\nprogram\n";
+    std::cout << "This\nis\na\nC++\nprogram\n";
+    std::cout << "Enter two numbers";
+    int b{}, c{};
+    std::cin >> b, c;
+    int a{b * c};
+    //This program preforms payroll calculations
+    std::cin >> a >> b >> c;
 }
